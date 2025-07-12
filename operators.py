2ad965e4e0c5eaf02f3caba98cd3290d04d6f2b4
Muraya-Pat <patrickmuraya823@gmail.com>
2025-07-12 17:28:54 +0300

    Add demonstration of various operators in Python

---
+++ operators.py
@@ -0,0 +1,32 @@
+# operators in python
+# Python supports various operators for arithmetic, comparison, logical operations, and more.
+# Arithmetic Operators
+# Arithmetic Operators
+num1 = 10
+num2 = 5
+sum_result = num1 + num2  # Addition
+diff_result = num1 - num2  # Subtraction
+prod_result = num1 * num2  # Multiplication
+div_result = num1 / num2  # Division
+mod_result = num1 % num2  # Modulus
+exp_result = num1 ** num2  # Exponentiation
+floor_div_result = num1 // num2  # This is floor division itreturns the largest integer less than or equal to the division result
+
+#comparison operators
+is_equal = (num1 == num2)  # Equal to
+is_not_equal = (num1 != num2)  # Not equal to
+is_greater = (num1 > num2)  # Greater than
+is_less = (num1 < num2)  # Less than
+is_greater_equal = (num1 >= num2)  # Greater than or equal to
+is_less_equal = (num1 <= num2)  # Less than or equal to
+# Logical Operators
+is_true = True
+is_false = False
+and_result = is_true and is_false  # Logical AND
+or_result = is_true or is_false  # Logical OR
+not_result = not is_true  # Logical NOT
+
+# assigment operators
+assignment = 10  # Assignment
+assignment += 5  # Add and assign
+assignment -= 3  # Subtract and assign / reduce assignment by 3 
