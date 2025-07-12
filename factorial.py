d9983aa54cb63d0f6591775e7a80be7447a5b5a3
Muraya-Pat <patrickmuraya823@gmail.com>
2025-07-12 17:08:18 +0300

    Add factorial functions using recursion and iteration

---
+++ factorial.py
@@ -0,0 +1,13 @@
+#factorial of a number with python
+# using recursion
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+# using iteration
+def factorial_iterative(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
