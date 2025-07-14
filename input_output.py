2e218c6ebdf849b609a6f85affa75869c9e5c359
Muraya-Pat <patrickmuraya823@gmail.com>
2025-07-14 17:12:15 +0300

    Add user input handling and formatted output demonstration

---
+++ input_output.py
@@ -0,0 +1,8 @@
+#User input in python
+num2 = input("Enter a number: ")
+num3 = int(num2)
+print (num3)
+
+# use template literals to print num3 with a statement before the value "num 3 : "
+print(f"num 3 : {num3}")
+sum = num3 + 10
